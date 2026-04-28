34a206caaa428ef259dfa031551863708c0cebb6
Иван Анатольевич Оберюхтин <io@oberukhtin.ru>
2026-04-28 23:47:39 +0500

    fix: Исправлен деплой - убран migrate-sqlite.js из build, добавлен deploy.sh

---
+++ messenger/deploy.sh
@@ -0,0 +1,14 @@
+# 1. Перейди в проект
+cd путь\к\твоему\проекту\Messenger_Balloo_next_ts
+
+# 2. Проверь статус Git
+git status
+
+# 3. Добавь все изменения
+git add .
+
+# 4. Сделай коммит
+git commit -m "fix: Исправлен деплой - убран migrate-sqlite.js из build, добавлен deploy.sh"
+
+# 5. Отправь на GitHub
+git push origin main
